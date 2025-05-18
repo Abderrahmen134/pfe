7ec012203ee31c8e6e48d221935701c2fe594c3f
Abderrahmen134 <abdoukchaou@gmail.com>
2025-05-18 15:16:49 +0200

    powerside

--- src/app/powerbi-report/powerbi-report.component.ts
+++ src/app/powerbi-report/powerbi-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-powerbi-report',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PowerbiReportComponent implements OnInit {
 
-  constructor() { }
+    
+  constructor(private router: Router) { }
 
+  notifications = {
+    devis: 0,
+    confirmation: 0,
+    confirme: 0,
+    livraison: 0,
+    livre: 0,
+    dashboard: 0,
+    produits: 0,
+    clients: 0,
+    admins: 0
+  };
   ngOnInit(): void {
   }
+get totalNotifications(): number {
+    return this.notifications.devis + 
+           this.notifications.confirmation +
+           this.notifications.confirme +
+           this.notifications.livraison +
+           this.notifications.livre;
+  }
 
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    localStorage.removeItem('admin'); // si tu stockes aussi l'admin/client
+  
+    this.router.navigate(['/']); // Redirige vers la page de connexion
+  }
 }
